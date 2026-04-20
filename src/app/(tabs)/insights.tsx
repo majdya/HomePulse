217b4c78e27d8100d2918dae8527ec33a9266f39
majdya <majd.ya.1995@gmail.com>
2026-04-20 09:23:00 +0300

    feat: implement insights screen components including stats, categories, priorities, and user profile

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,28 +1,22 @@
-// import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
-// import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
-// import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
-// import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
-// import SentryFeedbackButton from "@/components/insights/SentryFeedbackButton";
-// import UserProfile from "@/components/insights/UserProfile";
-// import TabScreenBackground from "@/components/TabScreenBackground";
-import { Text, View } from "react-native";
+import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
+import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
+import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
+import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
+import SentryFeedbackButton from "@/components/insights/SentryFeedbackButton";
+import UserProfile from "@/components/insights/UserProfile";
+import TabScreenBackground from "@/components/TabScreenBackground";
+import { ScrollView } from "react-native";
 
 const InsightsScreen = () => {
   return (
     <>
-      <View className="flex-1 items-center justify-center bg-background">
-        <Text>Insights Screen</Text>
-      </View>
-      {/* <ScrollView
-
-
+      <ScrollView
         className="flex-1 bg-background py-4"
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ padding: 20, gap: 14 }}
         contentInsetAdjustmentBehavior="automatic"
       >
         <TabScreenBackground />
-
         <UserProfile />
         <InsightsStatsSection />
         <InsightsCategorySection />
@@ -30,7 +24,7 @@ const InsightsScreen = () => {
         <ClearCompletedButton />
       </ScrollView>
 
-      <SentryFeedbackButton /> */}
+      <SentryFeedbackButton />
     </>
   );
 };
