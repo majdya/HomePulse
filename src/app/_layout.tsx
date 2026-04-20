7f1c16bcdba6b23eccee07f9b48d520f4f60923d
majdya <majd.ya.1995@gmail.com>
2026-04-20 09:23:30 +0300

    feat: integrate Sentry for error tracking and feedback collection

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,13 +10,37 @@ import { Stack } from "expo-router";
 import { useColorScheme } from "react-native";
 import "../../global.css";
 
+import * as Sentry from "@sentry/react-native";
+
+Sentry.init({
+  dsn: "https://00b8906801ec3e258dd510369222edd2@o4507287499767808.ingest.de.sentry.io/4511250854707280",
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
 if (!publishableKey) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-export default function RootLayout() {
+function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
@@ -26,3 +50,5 @@ export default function RootLayout() {
     </ClerkProvider>
   );
 }
+
+export default Sentry.wrap(RootLayout);
